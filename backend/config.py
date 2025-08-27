71cc09bc7c24f5a91b6ad9f4ee35cbd3c94650d5
Thompson Plyler <nealok.g@gmail.com>
2025-08-27 09:44:31 -0400

    --allow-empty

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,15 @@
 import os
+from sqlalchemy.pool import NullPool
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:password@localhost:5432/adam'
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    # In gevent/gunicorn environments, avoid SQLAlchemy's internal queue/locks
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'poolclass': NullPool,
+        'pool_pre_ping': True,
+    }
     # Auto-advance timers (seconds)
     GUESS_DURATION_SEC = int(os.environ.get('GUESS_DURATION_SEC', '20'))
     SCOREBOARD_DURATION_SEC = int(os.environ.get('SCOREBOARD_DURATION_SEC', '6'))
